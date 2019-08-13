76a68d92062b900e3b91c2a5de3c0030f9ef1c5a
XI327660 <federico.pacheco@grupoavalon.es>
2019-08-13 08:59:15 +0200

    Stack y Queue Genericos

---
+++ QueueGenericos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueGenericoSN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<CPunto> puntos = new Queue<CPunto>();
+
+            puntos.Enqueue(new CPunto(3,5));
+            puntos.Enqueue(new CPunto(8, 15));
+            puntos.Enqueue(new CPunto(-3, -1));
+            puntos.Enqueue(new CPunto(8, 9));
+
+            foreach(CPunto p in puntos)
+            {
+                Console.WriteLine(p);
+
+            }
+            Console.WriteLine("----------------");
+
+            //Hacemos un peek. (muestra el primer elemento pero no le saca)
+            Console.WriteLine("peek {0}", puntos.Peek());
+            foreach (CPunto p in puntos)
+            {
+                Console.WriteLine(p);
+
+            }
+            Console.WriteLine("----------------");
+
+
+            //Hacemos un par de dequeues. (muestra el primer elemento pero no le saca)
+            Console.WriteLine("dequeue {0}", puntos.Dequeue());
+            Console.WriteLine("dequeue {0}", puntos.Dequeue());
+            foreach (CPunto p in puntos)
+            {
+                Console.WriteLine(p);
+
+            }
+            Console.WriteLine("----------------");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StackGenerico/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackGenericoNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<CPunto> puntos = new Stack<CPunto>();
+
+            puntos.Push(new CPunto(3, 12));
+            puntos.Push(new CPunto(8, 4));
+            puntos.Push(new CPunto(5, 6));
+            puntos.Push(new CPunto(3, 11));
+
+            foreach(CPunto p in puntos)
+            {
+                Console.WriteLine("->{0}", p);
+   
+            }
+            Console.WriteLine("-------------");
+
+            //Hacemos un peek (no sale de stack)
+            Console.WriteLine("Peek {0}",puntos.Peek());
+
+            foreach (CPunto p in puntos)
+            {
+                Console.WriteLine("->{0}", p);
+
+            }
+            Console.WriteLine("-------------");
+
+            //Hacemos un par de pops (salen de stack)
+            Console.WriteLine("Pop {0}", puntos.Pop());
+            Console.WriteLine("Pop {0}", puntos.Pop());
+
+            foreach (CPunto p in puntos)
+            {
+                Console.WriteLine("->{0}", p);
+
+            }
+            Console.WriteLine("-------------");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
